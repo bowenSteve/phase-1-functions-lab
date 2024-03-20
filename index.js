da1952e9034951c5390825176821e71d504f34ad
Stephen Bowen <bowensteven17@gmail.com>
2024-03-20 09:41:28 +0300

    Completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    return Math.abs((location-42));
+}
+function distanceFromHqInFeet(someValue) {
+    let blocks = distanceFromHqInBlocks(someValue);
+    return blocks*264;
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination-start))*264;
+}
+function calculatesFarePrice(start, destination){
+    let feetTravelled=distanceTravelledInFeet(start,destination);
+    if (feetTravelled<=400){
+        return 0;
+    }else if(feetTravelled>400 && feetTravelled<=2000){
+        return ((feetTravelled-400)*2)/100;
+    }else if(feetTravelled>2000 && feetTravelled<=2500){
+       return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+
+}
